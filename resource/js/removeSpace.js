e38c09239f94ff609c376522f3268051d60ffb30
dekaifootwear <45461119+dekaifootwear@users.noreply.github.com>
2018-12-05 13:00:51 +0800

    modify removeSpace

--- resource/js/removeSpace.js
+++ resource/js/removeSpace.js
@@ -1,1 +1,6 @@
-$(document).ready(function() { $("body").children().each(function() { $(this).html($(this).html().replace(/&#8232 ;/g," ")); // remove the space in front of the first semicolon! }); });
+$(document).ready(function () {
+    $("body").children().each(function () {
+        $(this).html($(this).html().replace(/&#8232 ;/g, " "));
+    });
+});
+// remove the space in front of the first semicolon!
