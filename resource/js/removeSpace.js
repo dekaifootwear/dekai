ea0a857d128bbcbc80732b07faa96c9802645706
dekaifootwear <45461119+dekaifootwear@users.noreply.github.com>
2018-12-06 22:06:57 +0800

    20181206 修改 &#8232;

--- resource/js/removeSpace.js
+++ resource/js/removeSpace.js
@@ -1,6 +1,6 @@
+// remove the space in front of the first semicolon!
 $(document).ready(function () {
     $("body").children().each(function () {
-        $(this).html($(this).html().replace(/&#8232;/g, " "));
+        $(this).html($(this).html().replace("&#8232;", " "));
     });
 });
-// remove the space in front of the first semicolon!
